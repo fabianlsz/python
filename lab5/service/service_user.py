9f562c5d3c6317a1a0c37fba9144ca631001e65b
Fabian <fabianraul1904@gmail.com>
2024-12-10 11:32:05 +0200

    updated service to make sure there won't be 2 of the same user

--- lab5/service/service_user.py
+++ lab5/service/service_user.py
@@ -9,11 +9,13 @@ class Service_user:
         self.UserRepository = repoUser
 
     def add_user(self, name: str, age: int):
+        for user in self.UserRepository.get_all():
+            if user.name == name and user.age == age:
+                raise ValueError("Utilizatorul exista deja!")
         idUserNou = self.UserRepository.gen_id_user()
         userNou = User(idUserNou, name, age)
         self.Validator.validator_user(userNou)
         self.UserRepository.add(userNou)
-
     def remove_user(self, iduser: int):
         self.UserRepository.delete(iduser)
 
