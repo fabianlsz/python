729a33ab17d051ee84fee6bf6b3bda352172a4f9
fabianlsz <fabianraul1904@gmail.com>
2024-12-10 22:43:18 +0200

    finalised movie and user related work

--- lab5/domain/watchlist_domain.py
+++ lab5/domain/watchlist_domain.py
@@ -1,16 +1,16 @@
 class Watchlist:
-    def __init__(self, watchlist_id, user_id,movie_id):
+    def __init__(self, watchlist_id, user_id, movie_id):
         self.watchlist_id = watchlist_id
         self.user_id = user_id
         self.movie_id = movie_id
 
     @property
     def watchlist_id(self):
-        return self.watchlist_id
+        return self._watchlist_id
 
     @watchlist_id.setter
     def watchlist_id(self, value):
-        self.watchlist_id = value
+        self._watchlist_id = value
 
     def __str__(self):
         return f"Watchlist[ID={self.watchlist_id}, UserID={self.user_id}, Movies={self.movie_id}]"
